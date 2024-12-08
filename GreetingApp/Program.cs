41da8c360a5b7a6191ecb7356a457844a03f92e5
Michael Angelo Artiaga <tmq.michaelartiaga@gmail.com>
2024-12-08 22:50:06 +0800

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Welcome User to App
+Console.WriteLine("Welcom to the Greeting Application");
+Console.WriteLine("This application was build by Angelo Artiaga");
+Console.WriteLine("-------");
+Console.WriteLine();
+
+//Ask for First Name
+Console.Write("What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+//Greet user by Name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName);
+
+Console.WriteLine("Thank you for using application");
+Console.ReadLine();
